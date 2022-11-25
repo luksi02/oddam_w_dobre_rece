d69d290d5abfeaa1cb68e8a08528ce94afa7c294
luksi02 <lukaszgniewek@gmail.com>
2022-11-25 22:49:11 +0100

    2022_11_25_LG2

--- oddam/oddam_app/views.py
+++ oddam/oddam_app/views.py
@@ -2,13 +2,22 @@ from django.http import request
 from django.shortcuts import render
 from django.views import View
 
+from oddam_app.models import Donation
+
 
 # Create your views here.
 class IndexView(View):
 
     def get(self, request):
 
-        return render(request, 'index.html')
+        donations_list = Donation.objects.all()
+        liczba_oddanych_workow = 0
+        liczba_wspartych_organizacji = 0
+        for donation in donations_list:
+            liczba_oddanych_workow = liczba_oddanych_workow + donation.quantity
+            liczba_wspartych_organizacji = liczba_wspartych_organizacji + 1
+
+        return render(request, 'index.html', {'liczba_wspartych_organizacji':liczba_wspartych_organizacji, 'liczba_oddanych_workow':liczba_oddanych_workow})
 
 class LoginView(View):
 
