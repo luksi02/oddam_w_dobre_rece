f19ab965ffd3bd1347ec2f22615968a6482ae7fd
luksi02 <lukaszgniewek@gmail.com>
2022-12-07 01:15:45 +0100

    2022_12_07_LG1

--- oddam/oddam_app/views.py
+++ oddam/oddam_app/views.py
@@ -125,17 +125,20 @@ class ChangeUserSettingsView(View):
         user = get_object_or_404(User, id=id)
         return render(request, 'change_user_settings.html')
 
-    """def post(self, request, id):
-        first_name = request.POST.get("first_name")
-        last_name = request.POST.get("last_name")
-        username = request.POST.get("email")
+    def post(self, request, id):
+        user = get_object_or_404(User, id=id)
+        password_old = request.POST.get("password_old")
+        user = authenticate(request, username=user.username, password=password_old)
+
+        user.first_name = request.POST.get("first_name")
+        user.last_name = request.POST.get("last_name")
+        user.username = request.POST.get("email")
         password1 = request.POST.get("password")
         password2 = request.POST.get("password2")
         #user = authenticate(request, username=username, password=password)
-        if username and password1 == password2:
+        if password1 == password2:
             password = password1
-            user = User.objects.create(first_name=first_name, last_name=last_name, username=username)
             user.set_password(password)
             user.save()
             return redirect(reverse('login'))
-        return render(request, 'register.html', {'message':"Niepoprawne dane!"})"""
+        return render(request, 'register.html', {'message':"Niepoprawne dane!"})
